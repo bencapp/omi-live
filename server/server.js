4d1a6b0d81d05541193dde65e7bfa7255c17116f
Ben Capp <bentucker333@gmail.com>
2023-04-06 15:30:05 -0500

    created tables and updated users queries

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
 
-const sessionMiddleware = require('./modules/session-middleware');
-const passport = require('./strategies/user.strategy');
+const sessionMiddleware = require("./modules/session-middleware");
+const passport = require("./strategies/user.strategy");
 
 // Route includes
-const userRouter = require('./routes/user.router');
+const usersRouter = require("./routes/users.router");
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -22,10 +22,10 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 /* Routes */
-app.use('/api/user', userRouter);
+app.use("/api/user", usersRouter);
 
 // Serve static files
-app.use(express.static('build'));
+app.use(express.static("build"));
 
 // App Set //
 const PORT = process.env.PORT || 5000;
