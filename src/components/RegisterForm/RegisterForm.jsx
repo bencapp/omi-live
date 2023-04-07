3dee7b04fb59227c407405f3c5dd79eac809b7f3
Ben Capp <bentucker333@gmail.com>
2023-04-07 11:33:34 -0500

    removed comments

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -22,41 +22,6 @@ function RegisterForm() {
   }; // end registerUser
 
   return (
-    // <form className="formPanel" onSubmit={registerUser}>
-    //   <h2>Register User</h2>
-    //   {errors.registrationMessage && (
-    //     <h3 className="alert" role="alert">
-    //       {errors.registrationMessage}
-    //     </h3>
-    //   )}
-    //   <div>
-    //     <label htmlFor="username">
-    //       Username:
-    //       <input
-    //         type="text"
-    //         name="username"
-    //         value={username}
-    //         required
-    //         onChange={(event) => setUsername(event.target.value)}
-    //       />
-    //     </label>
-    //   </div>
-    //   <div>
-    //     <label htmlFor="password">
-    //       Password:
-    //       <input
-    //         type="password"
-    //         name="password"
-    //         value={password}
-    //         required
-    //         onChange={(event) => setPassword(event.target.value)}
-    //       />
-    //     </label>
-    //   </div>
-    //   <div>
-    //     <input className="btn" type="submit" name="submit" value="Register" />
-    //   </div>
-    // </form>
     <>
       <form onSubmit={registerUser}>
         <Box
