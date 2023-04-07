9c33622866058dd1fa4c7f7296e5f52b009020c6
Solomon Lambert <sol.lambert@gmail.com>
2023-04-07 14:27:33 -0500

    create nms and configure middleware for proxying connections to live stream server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const NodeMediaServer = require('node-media-server');
+const config = require('./constants/stream_config');
+const { createProxyMiddleware } = require('http-proxy-middleware')
 require('dotenv').config();
 
 const app = express();
@@ -30,7 +34,33 @@ app.use(express.static('build'));
 // App Set //
 const PORT = process.env.PORT || 5000;
 
+const allowedOrigins = ['http://localhost:3000'];
+
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
+const proxyOptions = {
+  target: `http://localhost:${process.env.STREAM_PORT || 5001}`,
+  changeOrigin: true,
+  ws: true,
+  router: {
+    [`http://localhost:${PORT}/live`]: `http://localhost:${process.env.STREAM_PORT || 5001}/live`,
+  }
+}
+app.use('/live', createProxyMiddleware(proxyOptions));
+
 /** Listen * */
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
+
+let nms = new NodeMediaServer(config)
+nms.run();
