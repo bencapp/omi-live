5fa7ea678c8ed25e2c70f7e8cd8f85342fc49b8a
Zakariye Abdi <zak.abdi94@gmail.com>
2023-04-10 14:00:46 -0500

    Adding functionality

--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -6,10 +6,10 @@ import {
   Typography,
   Box,
   TextareaAutosize,
+  CardMedia,
 } from "@mui/material";
 
 function InfoPage() {
-  const [title, setTitle] = useState("");
   const [url, setUrl] = useState("");
   const [description, setDescription] = useState("");
   const [couponCode, setCouponCode] = useState("");
@@ -20,87 +20,89 @@ function InfoPage() {
     e.preventDefault();
     dispatch({
       type: "ADD_PRODUCT",
-      payload: { title, url, description, couponCode },
+      payload: { url, description, couponCode },
     });
-    setTitle("");
     setUrl("");
     setDescription("");
     setCouponCode("");
   };
 
+  // const handleCopyCouponCode = () => {
+  //   navigator.clipboard.writeText(couponCode);
+  // };
+
   return (
     <form
       onSubmit={handleSubmit}
-      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        margin: "10px",
+      }}
     >
-      Title:
-      <TextField
-        id="standard-basic"
-        variant="standard"
-        type="text"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      URL:
-      <TextField
-        id="standard-basic"
-        variant="standard"
-        type="text"
-        value={url}
-        onChange={(e) => setUrl(e.target.value)}
-      />
-      <div
-        style={{
+      <Box
+        sx={{
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
         }}
       >
+        <Typography variant="h5">Add Product</Typography>
+        <Typography> URL: </Typography>
+        <TextField
+          id="standard-basic"
+          variant="standard"
+          type="text"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
         {url && (
           <img
             src={url}
             alt="Product Preview"
-            width={200}
-            height={200}
+            width={240}
+            height={240}
             style={{ marginTop: "20px" }}
           />
         )}
-        {description && (
-          <div
-            style={{
-              marginTop: "20px",
-              border: "1px solid #ccc",
-              padding: "10px",
+      </Box>
+      <div
+        style={{
+          marginTop: "20px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <label style={{ marginBottom: "60px" }}>
+          <Typography sx={{ margin: 1 }}> Description: </Typography>
+          <TextareaAutosize
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            sx={{
+              marginLeft: "10px",
+              padding: "5px",
               borderRadius: "3px",
+              border: "1px solid #ccc",
             }}
-          >
-            <Typography> Description: {description}</Typography>
-          </div>
-        )}
+          />
+        </label>
+        <label style={{ marginBottom: "20px" }}>
+          <Typography sx={{ margin: 1 }}> Coupon Code: </Typography>
+          <TextField
+            id="standard-basic"
+            variant="standard"
+            type="text"
+            value={couponCode}
+            onChange={(e) => setCouponCode(e.target.value)}
+          />{" "}
+          {/* <Button variant="contained" onClick={handleCopyCouponCode}>
+            Copy
+          </Button> */}
+        </label>
       </div>
-      <label style={{ marginBottom: "20px", marginTop: "20px" }}>
-        Description:
-        <TextareaAutosize
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          style={{
-            marginLeft: "10px",
-            padding: "5px",
-            borderRadius: "3px",
-            border: "1px solid #ccc",
-          }}
-        />
-      </label>
-      Coupon Code:
-      <TextField
-        id="standard-basic"
-        variant="standard"
-        type="text"
-        value={couponCode}
-        onChange={(e) => setCouponCode(e.target.value)}
-      />
-      <Button variant="contained" type="submit">
-        {" "}
+      <Button variant="contained" type="submit" sx={{ cursor: "pointer" }}>
         Add Product
       </Button>
     </form>
