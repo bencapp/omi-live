108b495d53d8edc9cd0624d24d6726eb6e18707b
Zakariye Abdi <zak.abdi94@gmail.com>
2023-04-10 15:17:27 -0500

    Created GET and POST route for the Add products view

--- server/routes/products.router.js
+++ server/routes/products.router.js
@@ -2,18 +2,41 @@ const express = require("express");
 const pool = require("../modules/pool");
 const router = express.Router();
 
-/**
- * GET route template
- */
-router.get("/", (req, res) => {
-  // GET route code here
+router.get("/", async (req, res) => {
+  const sqlQuery = "SELECT * FROM products";
+  try {
+    const result = await pool.query(sqlQuery);
+    const products = result.rows;
+    console.log("products", products);
+    res.send(products);
+  } catch (err) {
+    console.error(err);
+    res.status(500);
+  }
 });
 
-/**
- * POST route template
- */
-router.post("/", (req, res) => {
-  // POST route code here
+router.post("/", async (req, res) => {
+  try {
+    const {
+      name,
+      image_url,
+      description,
+      coupon_code,
+      coupon_expiration,
+      url,
+    } = req.body;
+    const client = await pool.connect();
+    const result = await client.query(
+      "INSERT INTO products (name, image_url, description, coupon_code, coupon_expiration, url) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      [name, image_url, description, coupon_code, coupon_expiration, url]
+    );
+    const newProduct = result.rows[0];
+    client.release();
+    res.status(201).json(newProduct);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error creating product" });
+  }
 });
 
 module.exports = router;
