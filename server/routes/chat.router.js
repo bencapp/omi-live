1674c688b9ebb45002341ecc5e825c9a5ac13fba
Andrea Love <andrealovelovelove@gmail.com>
2023-04-11 11:47:50 -0500

    added get and post endpoints for chat in router

--- server/routes/chat.router.js
+++ server/routes/chat.router.js
@@ -3,15 +3,46 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 /**
- * GET route template
+ * GET route
  */
-router.get('/', (req, res) => {
-  // GET route code here
+router.get('/:id', rejectUnauthenticated, (req, res) => {
+    const queryText = `SELECT "comments".text, "comments".timestamp, "users".username FROM "comments"
+                    JOIN "users" ON "users".id = "comments".user_id
+                    WHERE "comments".stream_id = 1
+                    ORDER BY "comments".timestamp DESC`;
+    const queryParams = [req.params.stream_id]
+    pool.query(queryText, queryParams)
+    .then(result => {
+        res.send(result.rows);
+    })
+    .catch(err => {
+        console.log('Get comments by id failed in server', err);
+        res.sendStatus(500)
+      })
 });
 
 /**
- * POST route template
+ * POST route
  */
+router.post("/", rejectUnauthenticated, (req, res) => {
+    console.log("in post text, req.body is", req.body);
+    const queryText = `INSERT INTO text ("stream_id", "user_id", "text", "timestamp")
+                       VALUES ($1, $2, $3, current_timestamp)`;
+    const queryParams = [req.body.stream_id, req.body.user_id, req.body.text];
+    pool.query(queryText, queryParams)
+      .then(() => {
+        req.io
+          .emit("add text", req.body.text, req.body.user_id);
+        res.sendStatus(204);
+      })
+      .catch((error) => {
+        console.log("Failed to execute SQL query:", queryText, " : ", error);
+        res.sendStatus(500);
+      });
+  });
+
+
+
 router.post('/', (req, res) => {
     const queryText =
     `INSERT INTO comments ("user_id", "text", "timestamp")
