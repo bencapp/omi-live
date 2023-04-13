ff348c5075afa1e3f7f27f3e857f05ac5df17d6a
Andrea Love <andrealovelovelove@gmail.com>
2023-04-13 09:42:44 -0500

    styled chat buttons

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -69,69 +69,80 @@ function Chat() {
     return (
         chatOpen === true ? (
             <Box
-            sx={{
-                backgroundColor: alpha(theme.palette.primary.main, 0.7),
-                borderRadius: "5px",
-                pt: "5px"
-            }}
-        >
-            <IconButton
-                size='large'
-                edge='start'
-                color='#000000'
-                aria-label='logo'
-                onClick={() => setChatOpen(false)}
                 sx={{
+                    backgroundColor: alpha(theme.palette.primary.main, 0.7),
+                    borderRadius: "5px",
+                    pt: "5px"
+                }}
+            >
+                <IconButton
+                    size='large'
+                    edge='start'
+                    color='#000000'
+                    aria-label='logo'
+                    onClick={() => setChatOpen(false)}
+                    sx={{
                         alignSelf: "stretch",
                         display: "flex",
                         justifyContent: "flex-end",
                         alignItems: "flex-start",
-                }}
-            >
-                <ArrowDropDownIcon />
-            </IconButton>
-            {allChats?.map(chat => {
-                return <Box
-                    key={chat.id}
-                    sx={{
-                        backgroundColor: alpha(theme.palette.secondary.main, 0.7),
-                        mx: "10px",
-                        my: "3px",
-                        p: "10px",
-                        borderRadius: "5px",
                     }}
                 >
-                    <Typography sx={{ fontSize: ".8em", fontWeight: "bold", display: "inline" }} >  {chat.username}: </Typography> <Typography component="div" sx={{ fontSize: ".8em", display: "inline" }}> {chat.text} </Typography>
-                    <Typography sx={{ fontSize: ".7em", fontStyle: 'italic' }}>  {dayjs(chat.timestamp).format('h:mm:ss A')} </Typography>
+                    <ArrowDropDownIcon />
+                </IconButton>
+                {allChats?.map(chat => {
+                    return <Box
+                        key={chat.id}
+                        sx={{
+                            backgroundColor: alpha(theme.palette.secondary.main, 0.7),
+                            mx: "10px",
+                            my: "3px",
+                            p: "10px",
+                            borderRadius: "5px",
+                        }}
+                    >
+                        <Typography sx={{ fontSize: ".8em", fontWeight: "bold", display: "inline" }} >  {chat.username}: </Typography> <Typography component="div" sx={{ fontSize: ".8em", display: "inline" }}> {chat.text} </Typography>
+                        <Typography sx={{ fontSize: ".7em", fontStyle: 'italic' }}>  {dayjs(chat.timestamp).format('h:mm:ss A')} </Typography>
+                    </Box>
+                })}
+                <Box sx={{ py: "10px" }} >
+                    <TextField sx={{ pl: "10px", width: 170, "& .MuiInputBase-root": { height: 40 }, }} pl="10px" id="outlined-basic" label="Chat here" variant="outlined" value={message} onChange={(event) => { setMessage(event.target.value) }} />
+                    <Button sx={{ ml: "15px", mr: "10px", fontSize: ".75em" }} size="small" onClick={sendMessage}>Send Message</Button>
                 </Box>
-            })}
-            <Box sx={{ py: "10px" }} >
-                <TextField sx={{ pl: "10px", width: 170, "& .MuiInputBase-root": { height: 40 }, }} pl="10px" id="outlined-basic" label="Chat here" variant="outlined" value={message} onChange={(event) => { setMessage(event.target.value) }} />
-                <Button sx={{ ml: "15px", mr: "10px", fontSize: ".75em" }} size="small" onClick={sendMessage}>Send Message</Button>
             </Box>
-        </Box>
         ) : (
-            <Box 
-            sx={{ ml: "10px", backgroundColor: theme.palette.primary.main }} >
-                <IconButton
-                size='large'
-                edge='start'
-                color='#000000'
-                aria-label='logo'
-                onClick={() => setChatOpen(true)}
+            <Box
                 sx={{
-                    alignSelf: "stretch",
-                    display: "flex",
-                    justifyContent: "flex-end",
-                    alignItems: "flex-start",
+                    borderRadius: "5px",
+                    backgroundColor: alpha(theme.palette.primary.main, 0.7),
+                    display: "flex"
                 }}
             >
-                   Chat
-                <ArrowDropUpIcon />
-            </IconButton>
+                    <Typography variant="body1">
+                        Chat
+                    </Typography>
+                    <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "center",
+                        alignSelf: "center",
+                        
+                    }}
+                    >
+                <IconButton
+                    size='large'
+                    edge='start'
+                    aria-label='logo'
+                    onClick={() => setChatOpen(true)}
+                    sx={{color: '#FFFFFF'}}
+                >
+                    <ArrowDropUpIcon />
+                </IconButton>
+                </Box>
             </Box>
         )
-       
+
     )
 
 }
