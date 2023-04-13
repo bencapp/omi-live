035da9a3c5b5e88543868f9d352affaa452ed7ae
Solomon Lambert <sol.lambert@gmail.com>
2023-04-13 14:14:23 -0500

    modify styling for chat and overlay on the stream

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -3,20 +3,42 @@ import Video from "../VideoJS/Video";
 import { useParams, useHistory } from "react-router-dom";
 import HomeIcon from "@mui/icons-material/Home";
 import { IconButton } from "@mui/material";
+import Chat from "../Chat/Chat";
 
 function UserPage() {
   const history = useHistory();
   const { username } = useParams();
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      <div style={{ backgroundColor: "#000000", height: "100vh" }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        height: "100vh",
+      }}
+    >
+      <div style={{ backgroundColor: "#000000", width: "100vw" }}>
         <Video username={username} />
       </div>
-      <div style={{ position: "absolute", zIndex: 1}}>
-        <IconButton variant="outlined" size="medium" sx={{ width: "1em", ml: ".75em", mt: ".4em" }}
-        onClick={() => history.push('/home')}>
+      <div style={{ position: "fixed", zIndex: 1 }}>
+        <IconButton
+          variant="outlined"
+          size="medium"
+          sx={{ width: "1em", ml: ".75em", mt: ".4em" }}
+          onClick={() => history.push("/home")}
+        >
           <HomeIcon color="secondary" />
         </IconButton>
+      </div>
+      <div
+        style={{
+          position: "fixed",
+          zIndex: 1,
+          alignSelf: "flex-end",
+          width: "100vw",
+        }}
+      >
+        <Chat />
       </div>
     </div>
   );
