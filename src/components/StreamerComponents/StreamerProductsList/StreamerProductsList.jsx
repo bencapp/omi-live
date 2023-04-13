3c7cfe8d8a9804ce71102dfb0a00d2192e0e0b2c
Zakariye Abdi <zak.abdi94@gmail.com>
2023-04-13 11:21:50 -0500

    fixed useSelector name

--- src/components/StreamerComponents/StreamerProductsList/StreamerProductsList.jsx
+++ src/components/StreamerComponents/StreamerProductsList/StreamerProductsList.jsx
@@ -4,7 +4,7 @@ import { useHistory } from "react-router";
 import { useEffect } from "react";
 
 function StreamerProductsList() {
-  const products = useSelector((store) => store.addProduct);
+  const products = useSelector((store) => store.allProduct);
 
   const history = useHistory();
   const dispatch = useDispatch();
