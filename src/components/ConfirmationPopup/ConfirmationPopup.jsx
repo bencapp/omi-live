db390567f15a92fba7d0ac473e6a1c57b139b664
Ben Capp <bentucker333@gmail.com>
2023-04-19 13:11:55 -0500

    completed popups

--- src/components/ConfirmationPopup/ConfirmationPopup.jsx
+++ src/components/ConfirmationPopup/ConfirmationPopup.jsx
@@ -6,38 +6,50 @@ function ConfirmationPopup({
   alertText,
   hidePopupText,
   confirmPopupText,
-  top,
 }) {
   return (
     <Box
       sx={{
-        //   height: "250px",
-        width: "80vw",
+        width: "100vw",
+        height: "100vh",
+        top: "0px",
+        left: "0px",
         position: "fixed",
-        top: top ? top : "",
-        left: "4.8vw",
-        borderRadius: "5px",
-        backgroundColor: "white",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        textAlign: "center",
-        gap: "20px",
-        padding: "20px",
-        fontSize: "1.3rem",
-        border: "1px solid black",
+        backgroundColor: "rgba(50, 50, 50, .5)",
         zIndex: "10",
       }}
     >
-      <Box>{alertText}</Box>
+      <Box
+        sx={{
+          width: "80vw",
+          margin: "41vh auto",
+          borderRadius: "5px",
+          backgroundColor: "white",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          textAlign: "center",
+          gap: "20px",
+          padding: "20px",
+          fontSize: "1.3rem",
+          border: "1px solid black",
+          zIndex: "100",
+        }}
+      >
+        <Box>{alertText}</Box>
 
-      <Box sx={{ display: "flex", gap: "15px", alignSelf: "center" }}>
-        <Button onClick={() => setDisplayConfirmation(false)}>
-          {hidePopupText}
-        </Button>
-        <Button color="warning" sx={{ color: "black" }} onClick={handleConfirm}>
-          {confirmPopupText}
-        </Button>
+        <Box sx={{ display: "flex", gap: "15px", alignSelf: "center" }}>
+          <Button onClick={() => setDisplayConfirmation(false)}>
+            {hidePopupText}
+          </Button>
+          <Button
+            color="warning"
+            sx={{ color: "black" }}
+            onClick={handleConfirm}
+          >
+            {confirmPopupText}
+          </Button>
+        </Box>
       </Box>
     </Box>
   );
